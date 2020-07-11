8c3e57c7054ab37ffb23c4ddf4d2c2d8e23fd0ff
SimranKak <68157161+SimranKak@users.noreply.github.com>
2020-07-11 20:57:37 +0530

    Add files via upload

---
+++ Images.java
@@ -0,0 +1,15 @@
+package com.b.truecitizenquiz;
+
+public class Images {
+    private String images;
+
+    public Images(String images) {
+        this.images=images;
+
+
+    }
+
+    public String getImages() {
+        return images;
+    }
+}
